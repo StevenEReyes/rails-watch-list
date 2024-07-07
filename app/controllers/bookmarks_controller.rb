25001e5c1f595c6d67d375cc354802d77e7a2631
Steven Reyes <steven.reyes88@gmail.com>
2024-07-08 00:17:05 +0900

    bookmark strong params not permitting on bookmark create

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,25 @@
 class BookmarksController < ApplicationController
   def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
   end
 
   def create
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new(bookmark_params)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
   has_many :bookmarks
-  validates :title, uniqueness: true, presence: true
+  validates :title, uniqueness: true
   validates :overview, presence: true
 end
